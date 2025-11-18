00a14cf952576de22601e442cb2549a2c3c8cdfa
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-11-18 20:55:39 +0300

    individual-task

--- WpfExample/WpfExample/MainWindow.xaml.cs
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfExample
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -30,13 +22,16 @@ namespace WpfExample
             }
             catch (Exception ex)
             {
-                this.Title = "Только целое число!";
+                this.Title = "Enter integer number only!";
+                return;
             }
             GenerateShapes(N);
         }
 
         private void GenerateShapes(int N)
         {
+            MainCanvas.Children.Clear();
+
             Random rndShapeType = new Random(DateTime.Now.Millisecond);
             Random rndStyle = new Random(DateTime.Now.Second);
             Random rndPosition = new Random(DateTime.Now.Minute);
@@ -45,29 +40,60 @@ namespace WpfExample
             for (int i = 0; i < N; i++)
             {
                 Shape currentShape;
-                int shapeType = rndShapeType.Next(0, 2);
-                if (shapeType == 0)
+                int shapeType = rndShapeType.Next(0, 4); // 4 типа фигур
+
+                switch (shapeType)
                 {
-                    currentShape = new Ellipse();
-                }
-                else
-                {
-                    currentShape = new Rectangle();
+                    case 0:
+                        currentShape = new Ellipse();
+                        break;
+                    case 1:
+                        currentShape = new Rectangle();
+                        break;
+                    case 2:
+                        currentShape = CreateTriangle();
+                        break;
+                    case 3:
+                        currentShape = new Polygon();
+                        ((Polygon)currentShape).Points = new PointCollection
+                        {
+                            new Point(0, 0),
+                            new Point(1, 1),
+                            new Point(0, 1)
+                        }; // Прямоугольный треугольник
+                        break;
+                    default:
+                        currentShape = new Ellipse();
+                        break;
                 }
 
-                int shapeStyle = rndStyle.Next(0, 3) + 1;
-                String styleName = "style" + shapeStyle.ToString();
+                // Выбор стиля (1-4)
+                int shapeStyle = rndStyle.Next(0, 4) + 1;
+                string styleName = "style" + shapeStyle;
                 Style currentStyle = (Style)this.FindResource(styleName);
                 currentShape.Style = currentStyle;
 
-                currentShape.Width = rndSize.Next(10, 200);
-                currentShape.Height = rndSize.Next(10, 100);
+                // Установка размеров
+                currentShape.Width = rndSize.Next(20, 150);
+                currentShape.Height = rndSize.Next(20, 150);
 
+                // Добавление на холст
                 MainCanvas.Children.Add(currentShape);
-                Canvas.SetLeft(currentShape, rndPosition.Next(5, 750));
-                Canvas.SetTop(currentShape, rndPosition.Next(5, 370));
+                Canvas.SetLeft(currentShape, rndPosition.Next(5, 700));
+                Canvas.SetTop(currentShape, rndPosition.Next(5, 250));
             }
         }
 
+        private Polygon CreateTriangle()
+        {
+            Polygon triangle = new Polygon();
+            triangle.Points = new PointCollection
+            {
+                new Point(0, 1),    // верхняя точка
+                new Point(1, -1),   // правая нижняя
+                new Point(-1, -1)   // левая нижняя
+            };
+            return triangle;
+        }
     }
 }
