4cfd84f036dbc72be4e0d2f7b9c437cae4a88f4e
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-11-20 09:34:40 +0300

    study-task

---
+++ Shop/DataTier/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace DataTier
+{
+    public class Product
+    {
+        public String Code {  get; set; }
+        public String Name { get; set; }
+        public float Price {  get; set; }
+        public int Count { get; set; }
+        public String Description {  get; set; }
+    }
+
+}
---
+++ Shop/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LogicTier;
+
+namespace PresentationTier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Shop logicTier = new Shop();
+            this.DataContext = logicTier;
+            InitializeComponent();
+        }
+    }
+}
